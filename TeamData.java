3fec8c0569eed1ba12d4435b7439dc79e9719400
Jeddmw <jwonohad@ucsc.edu>
2017-05-18 20:10:27 -0700

    Add files via upload

---
+++ TeamData.java
@@ -0,0 +1,19 @@
+//Object class to store home and away points for a team
+public class TeamData {
+	//Variables
+	double homePoints;
+	double awayPoints;
+	
+	//Constructor
+	public TeamData(double home, double away) 
+	{
+		this.homePoints = home;
+		this.awayPoints = away;
+	}
+	
+	//toString is called whenever TeamData is being printed out
+	public String toString()
+	{
+		return "Home points: " + homePoints + "\nAway points: " + awayPoints;
+	}
+}
