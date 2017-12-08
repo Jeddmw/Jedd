eef8e601e193aa22c75df4c00819ddefb9c6be97
Jeddmw <jwonohad@ucsc.edu>
2017-12-07 18:31:14 -0800

    Update TeamData.java

--- TeamData.java
+++ TeamData.java
@@ -3,17 +3,15 @@ public class TeamData {
 	//Variables
 	double homePoints;
 	double awayPoints;
-	
+
 	//Constructor
-	public TeamData(double home, double away) 
-	{
+	public TeamData(double home, double away) {
 		this.homePoints = home;
 		this.awayPoints = away;
 	}
-	
+
 	//toString is called whenever TeamData is being printed out
-	public String toString()
-	{
+	public String toString() {
 		return "Home points: " + homePoints + "\nAway points: " + awayPoints;
 	}
 }
